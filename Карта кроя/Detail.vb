3dd89ce9c3cb804015e7e97fa86a9d6dab6a4dee
Nikolay Davydov <nikolich253@icloud.com>
2016-11-20 22:30:19 +0300

    22.29

---
+++ Карта кроя/Detail.vb
@@ -0,0 +1,10 @@
+﻿Public Class Detail
+    Private Structure size
+        Private width As Integer
+        Private height As Integer
+    End Structure
+    Private sizedetail As size
+    Private countVertical As Integer = 1
+    Private countHorisontal As Integer = 1
+
+End Class
